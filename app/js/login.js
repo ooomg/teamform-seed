ee317d831160023ec5c03d9734a35b009f6a35a9
Gracewaiyin <wychuaf@connect.ust.hk>
2016-11-09 12:36:40 +0800

    fix the index page problem and the multiple login problem

--- app/js/login.js
+++ app/js/login.js
@@ -1,67 +1,7 @@
 // from http://blog.stackhive.com/post/114386894969/login-sign-up-and-a-real-time-app-using-firebase
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-/*
->>>>>>> 3ee1c345a80342646ee1721ac1d7448df73322eb
-=======
-/*
->>>>>>> 3ee1c345a80342646ee1721ac1d7448df73322eb
-var firebaseref = new Firebase("https://project-5db83.firebaseio.com")
-
-$("#login-btn").on('click', function()
-{
-    var email = $("#login-email").val();
-    var password = $("#login-password").val();
-    firebaseref.authWithPassword({
-        email: email,
-        password: password
-    },
-    function(error, authData) {
-        if (error) {
-            console.log("Login Failed!", error);
-        } else {
-            console.log("Authenticated successfully with payload:", authData);
-        }
-    });
-  });
-
-$("#signup-btn").on('click', function()
-{
-  var email = $("#email").val();
-  var password = $("#password").val();
-  firebaseref.createUser({
-    email: email,
-    password: password
-  },function(error, userData) {
-    if (error) {
-      console.log("Error creating user:", error);
-    }
-    else {
-      console.log("Successfully created user account with uid:", userData.uid);
-      //additionally, you can log the user in right after the signup is successful and add more data about the user like name etc.
-    }
-  });
-});
-//Callback for Auth Changes
-var authDataCallback = function(authData)
-{
-      //authData is the object sent by Firebase in the callback.
-      if (authData) {
-          console.log("User " + authData.uid + " is logged in");
-      }
-      else {
-          console.log("User is logged out");
-      }
-}
 
 //register a callback for the change in Authentication Status
 firebaseref.onAuth(authDataCallback);
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-=======
->>>>>>> 3ee1c345a80342646ee1721ac1d7448df73322eb
 */
   function toggleSignIn() {
       if (firebase.auth().currentUser) {
@@ -99,7 +39,7 @@ firebaseref.onAuth(authDataCallback);
       }
       document.getElementById('quickstart-sign-in').disabled = true;
     }
-    
+
     //fbpart
     // [START buttoncallback]
     function togglefbSignIn() {
@@ -273,7 +213,3 @@ firebaseref.onAuth(authDataCallback);
     window.onload = function() {
       initApp();
     };
-<<<<<<< HEAD
->>>>>>> 3ee1c345a80342646ee1721ac1d7448df73322eb
-=======
->>>>>>> 3ee1c345a80342646ee1721ac1d7448df73322eb
