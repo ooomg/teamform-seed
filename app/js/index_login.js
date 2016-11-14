57796c46010977c656c8b5fbce44a548681b12a0
cswongaq <cswongaq@ust.hk>
2016-11-14 15:56:54 +0800

    Create index_login.js

---
+++ app/js/index_login.js
@@ -0,0 +1,59 @@
+(function(){
+  var config = {
+    apiKey: "AIzaSyCYkV0-0g47RwZcLh4VbOOT1FtTqnyAuNA",
+    authDomain: "comp3111-project-c4f22.firebaseapp.com",
+    databaseURL: "https://comp3111-project-c4f22.firebaseio.com",
+    storageBucket: "comp3111-project-c4f22.appspot.com",
+    messagingSenderId: "801945913687"
+  };
+  firebase.initializeApp(config);
+
+  //menu login section
+  var menu_email = document.getElementById('menu_email');
+  var menu_password = document.getElementById('menu_password');
+  var menu_btnlogin = document.getElementById('menu_btnlogin');
+
+  //Sign up section variable
+  var txtName = document.getElementById('txtName');
+  var txtEmail = document.getElementById('txtEmail');
+  var txtPassword = document.getElementById('txtPassword');
+  var btnLogin = document.getElementById('btnLogin');
+  var btnSignup = document.getElementById('btnSignUp');
+
+  btnLogin.addEventListener('click', e=>{
+
+    var email = txtEmail.value;
+    var password = txtPassword.value;
+    var auth = firebase.auth();
+
+
+  });
+
+
+  //sign up event
+  btnSignup.addEventListener('click', e=>{
+
+    var email = txtEmail.value;
+    var password = txtPassword.value;
+    var name = txtName.value;
+    var auth = firebase.auth();
+    auth.createUserWithEmailAndPassword(email,password);
+    console.log('haha');
+
+    //TODO: get user uid, then create the profile of the users
+    //idk how to get the uid after call the funtion: createUserWithEmailAndPassword()
+
+
+    // var createAccount = firebase.database().ref('users/' + user.uid);
+    //
+    // createAccount.set({
+    //   Name: name,
+    //   Email: email
+    // });
+    //
+    // createAccount.set({
+    //   EventName: null
+    // });
+
+  });
+}()) ;
