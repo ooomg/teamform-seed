298b0408fb2378d9e462f07801b21f5f0e96a55a
XDrzzz <dannyng10231996@gmail.com>
2016-11-16 02:01:17 +0800

    Change a function
    change a new function '$scope.paramLdMess'

--- app/js/admin.js
+++ app/js/admin.js
@@ -32,25 +32,27 @@ angular.module('teamform-admin-app', ['firebase'])
 
 	// Link and sync a firebase object
 	$scope.param = $firebaseObject(ref);
-	$scope.param.$loaded()
-		.then(function(data) {
+	$scope.paramMess="";
+	$scope.paramLdMess=function() {
 
 			// Fill in some initial values when the DB entry doesn't exist
 			if(typeof $scope.param.maxTeamSize == "undefined"){
 				$scope.param.maxTeamSize = 10;
+				$scope.paramMess+="NoMax ";
 			}
 			if(typeof $scope.param.minTeamSize == "undefined"){
 				$scope.param.minTeamSize = 1;
+				$scope.paramMess+="NoMin ";
 			}
 
 			// Enable the UI when the data is successfully loaded and synchornized
 			$('#admin_page_controller').show();
-		})
-		.catch(function(error) {
+		};
+/*		.catch(function(error) {
 			// Database connection error handling...
 			//console.error("Error:", error);
 		});
-
+*/
 
 	refPath = eventName + "/team";
 	$scope.team = [];
