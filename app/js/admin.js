f26fac773580daeeaf37c8fe3a2b9aee3f642fc6
XDrzzz <dannyng10231996@gmail.com>
2016-11-16 02:24:40 +0800

    Minor fix to load the stuff out on load
    stated as title

--- app/js/admin.js
+++ app/js/admin.js
@@ -33,26 +33,30 @@ angular.module('teamform-admin-app', ['firebase'])
 	// Link and sync a firebase object
 	$scope.param = $firebaseObject(ref);
 	$scope.paramMess="";
-	$scope.paramLdMess=function() {
+	$scope.paramLdMess=function(){
+//	$scope.param.$loaded()
+//		.then(function(data) {
 
 			// Fill in some initial values when the DB entry doesn't exist
 			if(typeof $scope.param.maxTeamSize == "undefined"){
 				$scope.param.maxTeamSize = 10;
-				$scope.paramMess+="NoMax ";
+				$scope.paramMess+="NoMax "
 			}
 			if(typeof $scope.param.minTeamSize == "undefined"){
 				$scope.param.minTeamSize = 1;
-				$scope.paramMess+="NoMin ";
+				$scope.paramMess+="NoMin "
 			}
 
 			// Enable the UI when the data is successfully loaded and synchornized
 			$('#admin_page_controller').show();
 		};
-/*		.catch(function(error) {
+		$scope.param.$loaded().then($scope.paramLdMess);
+//		})
+//		.catch(function(error) {
 			// Database connection error handling...
 			//console.error("Error:", error);
-		});
-*/
+//		});
+
 
 	refPath = eventName + "/team";
 	$scope.team = [];
